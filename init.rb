8fda054e0e8ba02b1c2dfb9f6d765db2f0f69a9c
George Avramidis <avramidg@gmail.com>
2011-08-03 11:36:23 +0300

    fixed locale, date is now correct

--- init.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'db_models'
-require_relative 'ui'
-
-
-
--- main.rb
+++ main.rb
@@ -3,10 +3,15 @@ require_relative 'ui/main_window.rb'
 
 require_relative 'ui'
 
+lang = Qt::Locale::Greek
+country = Qt::Locale::Greece
+Qt::Locale.setDefault(Qt::Locale.new(lang, country))
+
 app = Qt::Application.new(ARGV)
 #Qt.debug_level = Qt::DebugLevel::High
 puts "Main: #{ $lala.inspect}"
 $lala = 7
+
 
 $mainWindow = MainWindow.new
 $mainWindow.setup_ui
